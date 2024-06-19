f0c1ce93d66416438c98d51f05f917cab49d2529
lixoO0 <153216405+lixoO0@users.noreply.github.com>
2024-06-19 13:07:51 +0300

    lab 10 done

--- blog/app/Models/BlogPost.php
+++ blog/app/Models/BlogPost.php
@@ -8,18 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class BlogPost extends Model
 {
-use SoftDeletes;
-protected $fillable
-        = [
-            'title',
-            'slug',
-            'category_id',
-            'excerpt',
-            'content_raw',
-            'is_published',
-            'published_at',
-            'user_id',
-        ];
+    use SoftDeletes;
+    use HasFactory;
+
+    const UNKNOWN_USER = 1;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'category_id',
+        'excerpt',
+        'content_raw',
+        'is_published',
+        'published_at',
+    ];
 
     /**
      * Категорія статті
@@ -28,7 +30,7 @@ protected $fillable
      */
     public function category()
     {
-        //стаття належить категорії
+        // стаття належить категорії
         return $this->belongsTo(BlogCategory::class);
     }
 
@@ -39,8 +41,7 @@ protected $fillable
      */
     public function user()
     {
-        //стаття належить користувачу
+        // стаття належить користувачу
         return $this->belongsTo(User::class);
     }
-    use HasFactory;
 }
